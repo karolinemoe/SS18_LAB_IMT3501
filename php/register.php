cf72cae204eb7699eeea6235436b84137821442e
andersgj <andersgj@github.com>
2018-10-01 11:41:30 +0200

    Added Email Filter validation for mail

--- php/register.php
+++ php/register.php
@@ -27,6 +27,6 @@
     $data['email'] = $_POST['email'];
 
     $res = $user->newUser($data);
-    if ($res['status'] == 'OK') header('Location: index.php');
+    if ($res['status'] == 'OK' && filter_var($data['email'], FILTER_VALIDATE_EMAIL)  ) header('Location: index.php');
     else echo $twig->render('register.html', array($res));
   }
